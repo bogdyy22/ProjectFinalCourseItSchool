bf53d601b4c6bcaf720d0267ed2a2d604fe6f556
Bogdan_Pacioianu <89405443+bogdyy22@users.noreply.github.com>
2021-09-28 11:15:20 +0300

    Update VariantaCompact.java

--- src/main/java/cars/config/entity/VariantaCompact.java
+++ src/main/java/cars/config/entity/VariantaCompact.java
@@ -21,7 +21,7 @@ import lombok.ToString;
 @Getter
 @ToString
 
-public class VariantaCompact extends Car implements Serializable  {
+public class VariantaCompact extends Car implements Serializable {
 
 	@Id
 	@Column(name = "Id")
@@ -32,7 +32,7 @@ public class VariantaCompact extends Car implements Serializable  {
 	@Column(name = "Variant_Car")
 
 	private CompactVariant compactVariant;
-	
+
 	@Column(name = "Horse_Power")
 	private int hp;
 
